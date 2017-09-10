536221924563d1513984bdcc6bc5ce7437f9880c
ahmadk3 <ahmad.aktb@gmail.com>
2017-09-09 23:19:49 -0300

    Bow fix

---
+++ Assets/Scripts/Cards/CardBow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBow : MonoBehaviour {
+    public GameObject arrow;
+    public float cooldown = 1f;
+
+    private Transform t;
+    private float nextfire = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.position += new Vector3(1, 1, 1);
+        t.position -= new Vector3(1, 1, 1);
+    }
+
+    void OnTriggerStay2D(Collider2D other){
+
+        if (other.CompareTag("Enemy") && Time.time > nextfire )
+        {
+            nextfire = Time.time + cooldown;
+
+            GameObject g = Instantiate(this.arrow);
+            g.active = true;
+            Transform t = g.GetComponent<Transform>();
+            Rigidbody2D rb = g.GetComponent<Rigidbody2D>();
+            //Vector3 force = new Vector3(10, 0, 0);
+            rb.AddForce((other.transform.position - this.t.position) * 5, ForceMode2D.Impulse);
+            t.position = this.t.position;
+            //t.LookAt(other.GetComponent<Transform>());
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/teste.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teste : MonoBehaviour {
+
+
+    private Transform t;
+    public GameObject g;
+    private float cooldown = 0.3f;
+    private float nextfire = 0;
+
+    // Use this for initialization
+    void Start () {
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("k"))
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (Input.GetKey("l") && Time.time > this.nextfire)
+        {
+            GameObject g2 = Instantiate(this.g);
+            Transform t2 = g2.GetComponent<Transform>();
+            t2.position = t.position;
+            nextfire = Time.time + this.cooldown;
+        }
+
+    }
+}
