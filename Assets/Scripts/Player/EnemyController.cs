7404cf95288e418dcafd52b9d117bc9bb6e804ea
ahmadk3 <ahmad.aktb@gmail.com>
2017-10-01 17:20:18 -0300

    healthbar finished

--- Assets/Scripts/Player/EnemyController.cs
+++ Assets/Scripts/Player/EnemyController.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyController : Player {
 
+    private Image healthBar;
+    public GameObject healthBarObject;
+
 	// Use this for initialization
 	void Start () {
-		
+        healthBar = healthBarObject.GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(this.health <= 0.0f)
+        healthBar.fillAmount = getHealthPercentage();
+
+        if (this.health <= 0.0f)
         {
             Destroy(this.gameObject);
         }
