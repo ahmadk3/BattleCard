643a2c0cc8b8ea7c481bcb2c6855836eca9b7e20
marco <marco.csalerno@gmail.com>
2017-10-05 19:03:23 -0300

    game UI 1.0

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Experimental.UIElements;
 
 public class PlayerController : Player {
+
+	public int startingHealth = 100;                            // The amount of health the player starts the game with.
+	public int currentHealth;                                   // The current health the player has.
+	public Slider healthSlider;                                 // Reference to the UI's health bar.
+	public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+	public AudioClip deathClip;                                 // The audio clip to play when the player dies.
+	public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
+	public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+
+	bool isDead;                                                // Whether the player is dead.
+	bool damaged; 
+
+
 
     public float jumpSpeed = 5.0f;
     public float firerate = 0.3f;
