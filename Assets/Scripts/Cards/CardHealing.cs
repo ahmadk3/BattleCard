933aac9b149dcb2c96cf51bf10608db9b7b98347
pamepeixinho <pameiupip@gmail.com>
2017-10-12 23:09:28 -0300

    first take in healing

--- Assets/Scripts/Cards/CardHealing.cs
+++ Assets/Scripts/Cards/CardHealing.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class CardHealing : Card {
 
+	private int healthPoints;
+
 	// Use this for initialization
 	void Start () {
 		this.cooldown = 1;
+		this.healthPoints = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		
+	void OnTriggerEnter2D() {
+		GameObject playerObj = GameObject.FindWithTag("Player");
+		Player player = playerObj.GetComponent<Player>();
+
+		if (player != null) {
+			player.health -= this.healthPoints;
+		}
 	}
 }
