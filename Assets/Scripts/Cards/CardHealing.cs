2a3501ceb22396218cd9f253122cda41809c0b15
pamepeixinho <pameiupip@gmail.com>
2017-10-13 00:05:15 -0300

    working - but wip

--- Assets/Scripts/Cards/CardHealing.cs
+++ Assets/Scripts/Cards/CardHealing.cs
@@ -10,23 +10,10 @@ public class CardHealing : Card {
 	void Start () {
 		this.cooldown = 1;
 		this.healthPoints = 10;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
 
-	void OnTriggerEnter2D() {
-		print("ESTOU AQUI");
-//		Player player = gameObject.GetComponent<Player> () as Player;
-//		Player p = gameObject.Find(Player);
-
-//		GameObject playerObj = GameObject.FindWithTag("Player");
-//		Player player = playerObj.GetComponent<Player>();
-//		print(player);
-//		if (p != null) {
-//			print("ESTOU AQUI");
-//			p.health -= 50;
-//		}
+		Player player = GameObject.Find ("Player").GetComponent<Player> ();
+		if (player != null) {
+			player.health = 0;
+		}
 	}
 }
