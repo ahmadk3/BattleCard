82ba378b7f1bdb564bd8349ba9c3ac13a52427ac
pamepeixinho <pameiupip@gmail.com>
2017-10-13 00:14:58 -0300

    fix health

--- Assets/Scripts/Cards/CardHealing.cs
+++ Assets/Scripts/Cards/CardHealing.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class CardHealing : Card {
 
-	private int healthPoints;
+	public int healthPoints;
 
 	// Use this for initialization
 	void Start () {
 		this.cooldown = 1;
-		this.healthPoints = 10;
 
 		Player player = GameObject.Find ("Player").GetComponent<Player> ();
 		if (player != null) {
-			player.health = 0;
+			player.health += healthPoints;
 		}
 	}
 }
